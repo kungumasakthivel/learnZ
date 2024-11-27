f493f3f48f4d71c8c56abc3cf6b5f4ca6729136c
kungumasakthivel <kungumasakthivel.k@gmail.com>
2024-11-27 13:24:27 +0530

    added contact form and tostify and .env for api key

--- apps/frontend/src/pages/Footer/Footer.jsx
+++ apps/frontend/src/pages/Footer/Footer.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import './Footer.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
 
-    const openEmail = () => {
-        window.open("mailto:kungumasakthivel.k@gmail.com?subject=Enquery%20about%20learnZ");
-    }
   return (
     <div className='footer-container'>
       <div className="footer-text">
         <p>&copy; learn<span className='footer-z'>Z</span></p>
       </div>
       <div className="social-icons">
-        <div className='icon-email' onClick={openEmail}>
-            <span>Email us: </span><FontAwesomeIcon icon={faEnvelope} />
+        <div>
         </div>
       </div>
     </div>
--- apps/frontend/src/pages/Landing/Landing.jsx
+++ apps/frontend/src/pages/Landing/Landing.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Landing.css'
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function Landing() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  const handleContact = async() => {
+    if(!name) {
+      return toast.error('Please enter name', {theme: "dark"})
+    }
+    if(!validateEmail(email)) {
+      return toast.error('Please enter a valid email', {theme: "dark"})
+    }
+    if(!message) {
+      return toast.error('Please enter message', {theme: "dark"})
+    }
+
+    const body = {name, email, message}
+
+    const res = await fetch(import.meta.env.VITE_POST_CONTACT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    if(!res.ok) {
+      toast.warn(res.message, {theme: "dark"});
+    }
+    if(res.ok) {
+      toast.success('We will contact you shortly soon!', {theme: "dark"});
+    }
+    setName('')
+    setEmail('')
+    setMessage('')
+
+  }
+
   return (
     <div className='landing-page-container'> 
       <Navbar />
@@ -33,8 +77,31 @@ function Landing() {
         </div>
       </div>
 
+      <div className="landing-body-container-3">
+        <div className="left">
+          <h1>Interested</h1>
+          <h2>Schedule a call</h2>
+          <h2>with us!</h2>
+        </div>
+        <div className="right">
+          <div className='form-outline'>
+            <input value={name} type='text' placeholder='Name'
+             required onChange={(e) => setName(e.target.value)}
+            />
+            <input value={email} type='email' placeholder='Email' 
+             required onChange={(e) => setEmail(e.target.value)} 
+            />
+            <textarea value={message} type='text' placeholder='Message' 
+             required onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={handleContact}>Submit</button>
+          </div>
+        </div>
+      </div>
+
       {/* footer */}
       <Footer />
+      <ToastContainer/>
     </div>
   )
 }
